d21bb7ebf8aeae014cff453dc5d184d313bae6de
miltonmb <miltonmb@unitec.edu>
2017-09-14 04:06:57 -0600

    Splash screen working

--- app/src/main/java/com/Demo/googlemapdemo/MapsActivity.java
+++ app/src/main/java/com/Demo/googlemapdemo/MapsActivity.java
@@ -1,70 +1,104 @@
 package com.Demo.googlemapdemo;
-
 import android.Manifest;
+
 import android.app.ProgressDialog;
+
 import android.content.pm.PackageManager;
+
 import android.location.Geocoder;
+
 import android.location.Location;
+
 import android.location.LocationListener;
+
 import android.location.LocationManager;
+
 import android.support.v4.app.ActivityCompat;
+
 import android.support.v4.app.FragmentActivity;
+
 import android.os.Bundle;
+
 import android.view.View;
+
 import android.widget.Button;
 
+
+
 import com.google.android.gms.maps.CameraUpdate;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
+
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
+
 import com.google.android.gms.maps.SupportMapFragment;
+
 import com.google.android.gms.maps.model.LatLng;
+
 import com.google.android.gms.maps.model.Marker;
+
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
+
 import java.io.IOException;
+
 import java.util.List;
 
+
+
 import Modules.DirectionFinderListener;
+
 import Modules.Route;
+
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, DirectionFinderListener {
 
+
+
     private GoogleMap mMap;
+
     private Button btnMarcar;
+
     private Button btVerFeed;
+
     private ProgressDialog progressDialog;
+
     private LocationManager location;
+
     private Marker currLocationMarker;
 
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
-        btnMarcar = (Button) findViewById(R.id.btMarcar);
-        btnMarcar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendRequest();
-            }
-        });
+
+
         location = (LocationManager) getSystemService(LOCATION_SERVICE);
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
+
         if (location.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
             location.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+
                 @Override
                 public void onLocationChanged(Location location) {
                     if (currLocationMarker != null) {
@@ -97,13 +131,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 }
 
+
+
                 @Override
                 public void onProviderDisabled(String provider) {
 
                 }
+
             });
+
         } else if (location.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             location.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+
                 @Override
                 public void onLocationChanged(Location location) {
                     if (currLocationMarker != null) {
@@ -124,7 +163,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                 }
 
                 @Override
@@ -141,14 +179,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 public void onProviderDisabled(String provider) {
 
                 }
+
             });
         }
-
     }
 
     private void sendRequest() {
 
     }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -158,14 +197,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             return;
+
         }
         mMap.setMyLocationEnabled(true);
-
     }
-
 
     @Override
     public void onDirectionFinderStart() {
+
     }
 
     @Override
